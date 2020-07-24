fadccbd90a4b8482cf486a940e2397704756ddc7
Jack Cooper <jaalcoop@ucsc.edu>
2020-07-23 20:37:34 -0700

    structure of tree start

--- Ogar3-master/src/ai/BehaviorTree.js
+++ Ogar3-master/src/ai/BehaviorTree.js
@@ -1,53 +1,159 @@
-var BotPlayer = require('../BotPlayer');
-var BTBot = require('../BTBot');
-const { BehaviorTree, Sequence, Task, SUCCESS, FAILURE } = require('behaviortree')
-const { BehaviorTree, Sequence, Task, SUCCESS, FAILURE, decorators: { AlwaysSucceedDecorator } } = require('behaviortree')
+import { BehaviorTree, Selector, Sequence, Task, SUCCESS, FAILURE } from 'behaviortree'
 
-// initialize and create Behavior Tree
-import { BehaviorTree } from 'behaviortree'
-var bTree = new BehaviorTree({
-  tree: mySelector,
-  blackboard: {}
-})
-
-// selector nodes
-import { Random } from 'behaviortree'
-const mySelector = new Random({
-  nodes: [
-    // here comes in a list of nodes (Tasks, Sequences or Priorities)
-    // as objects or as registered strings
-  ]
-})
-
-// sequence nodes
-import { Sequence } from 'behaviortree'
-const mySequence = new Sequence({
-  nodes: [
-    // here comes in a list of nodes (Tasks, Sequences or Priorities)
-    // as objects or as registered strings
-  ]
-})
-
-// tasks/leaf nodes
-import { Task, SUCCESS } from 'behaviortree'
-const myTask = new Task({
-  // (optional) this function is called directly before the run method
-  // is called. It allows you to setup things before starting to run
-  // Beware: if task is resumed after calling this.running(), start is not called.
-  start: function(blackboard) { blackboard.isStarted = true; },
-
-  // (optional) this function is called directly after the run method
-  // is completed with either this.success() or this.fail(). It allows you to clean up
-  // things, after you run the task.
-  end: function(blackboard) { blackboard.isStarted = false; },
-
-  // This is the meat of your task. The run method does everything you want it to do.
-  run: function(blackboard) {
+BehaviorTree.register('flee', new Task({
+  run: function (Bot) {
+    Bot.flee()
     return SUCCESS
   }
+}))
+
+BehaviorTree.register('fight', new Task({
+  run: function (Bot) {
+    Bot.fight()
+    return SUCCESS
+  }
+}))
+
+BehaviorTree.register('feed', new Task({
+  run: function (Bot) {
+    Bot.feed()
+    return SUCCESS
+  }
+}))
+
+BehaviorTree.register('wander', new Task({
+  run: function (Bot) {
+    Bot.wander()
+    return SUCCESS
+  }
+}))
+
+const tree = new Selector(
+{
+      nodes: [
+        // flee selector
+        new Selector(
+        {
+          run: function (Bot) 
+          {
+              // case 1: see a larger massed enemy near
+              if (Bot.largeMassNear())
+              {
+                  // case 1.1: predator is less than twice the size of us
+                  if (Bot.largerMassNearX2())
+                  {
+                      Bot.move()
+                      return SUCCESS
+                  }
+                  // case 1.2: predator is greater than twice the size of us
+                  /*
+                  else
+                  {
+                    Bot.split()
+                    return SUCCESS
+                  }
+                  */
+              } 
+              else 
+              {
+                  return FAILURE
+              }
+          }
+        }
+        ),
+        // fight sequence
+        new Selector({
+          run: function (Bot) {
+            // case 2: see a smaller massed enemy
+            if (Bot.smallMassNear()) 
+            {
+                // case 2.1: enemy mass is 1/4 < x < 1/2 our mass
+                /*if (Bot.smallMassNearSplitDecide())
+                {
+                    Bot.splitAttack()
+                    return SUCCESS
+                }
+                // case 2.2: enemy is 1/2 x < 1 our mass
+                else
+                { */
+                    Bot.chase()
+                    return SUCCESS
+               // }
+            } 
+            else 
+            {
+              return FAILURE
+            }
+          }
+        }
+        // feed selector
+        new Selector(
+        {
+          run: function (Bot) 
+          {
+              // case 3: see food
+              if (Bot.foodInRange())
+              }
+                    Bot.feed()
+                    return SUCCESS
+              } 
+              else 
+              {
+                  return FAILURE
+              }
+          }
+        }
+        ),
+        // wander - default if all else fails
+        new Selector(
+        {
+          run: function (Bot) 
+          {
+                    Bot.move()
+                    return SUCCESS
+              } 
+
+          }
+       
+          )
+        )
+      ]
+}
+)
+
+// implement actions here
+class Bot {
+    // behaviors
+  flee () {
+    //
+  }
+
+  fight () {
+    //
+  }
+
+  feed () {
+    //
+  }
+  
+  wander () {
+    //
+  }
+  
+    // checkers for state
+  standBesideATree () {
+    return true
+  }
+}
+
+const Bot = new Bot() 
+
+const bTree = new BehaviorTree({
+  tree: tree,
+  blackboard: Bot
 })
 
-// Run Tree:
-setInterval(function() {
+// The "game" loop:
+setInterval(function () {
   bTree.step()
-}, 1000/60)
+}, 1000 / 60)
