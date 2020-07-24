32651bb8a5630842172288aad4ac0e28433e5e51
Jack Cooper <jaalcoop@ucsc.edu>
2020-07-24 02:57:45 -0700

    prior to restructure of BT

--- Ogar3-master/src/ai/BTBot.js
+++ Ogar3-master/src/ai/BTBot.js
@@ -18,121 +18,114 @@ function BTBot() {
 module.exports = BTBot;
 BTBot.prototype = new PlayerTracker();
 
-// implement actions here
-class BehaviorBot {
-    // actions
-    flee() {
-        var avoid = this.predators[0];
-        //console.log("[Bot] "+cell.getName()+": Fleeing from "+avoid.getName());
-    
-        // Find angle of vector between cell and predator
-        var deltaY = avoid.position.y - cell.position.y;
-        var deltaX = avoid.position.x - cell.position.x;
-        var angle = Math.atan2(deltaX,deltaY);
-    
-        // Now reverse the angle
-        if (angle > Math.PI) {
-            angle -= Math.PI;
-        } else {
-            angle += Math.PI;
-        }
-    
-        // Direction to move
-        var x1 = cell.position.x + (500 * Math.sin(angle));
-        var y1 = cell.position.y + (500 * Math.cos(angle));
-    
-        if ((!this.target) || (this.target.getType() == 0) || (this.visibleNodes.indexOf(this.target) == -1)) {
-            var foods = this.getFoodBox(x1,y1);
-            if (foods) {
-                this.target = this.findNearest(cell, this.food);
-        
-                this.mouseX = this.target.position.x;
-                this.mouseY = this.target.position.y;
-            }
-        }   
-    
-        this.mouse = {x: x1, y: y1};
+BTBot.flee = function(){
+    var avoid = this.predators[0];
+    //console.log("[Bot] "+cell.getName()+": Fleeing from "+avoid.getName());
+
+    // Find angle of vector between cell and predator
+    var deltaY = avoid.position.y - cell.position.y;
+    var deltaX = avoid.position.x - cell.position.x;
+    var angle = Math.atan2(deltaX,deltaY);
+
+    // Now reverse the angle
+    if (angle > Math.PI) {
+        angle -= Math.PI;
+    } else {
+        angle += Math.PI;
     }
 
-    fight()  {
-        if ((!this.target) || (this.visibleNodes.indexOf(this.target) == -1)) {
-            this.target = this.getRandom(this.prey);
-        }
-        //console.log("[Bot] "+cell.getName()+": Targeting "+this.target.getName());
-                    
-        this.mouse = {x: this.target.position.x, y: this.target.position.y};
-    
-        var massReq = 1.25 * (this.target.mass * 2 ); // Mass required to splitkill the target
-    
-        if ((cell.mass > massReq) && (this.cells.length <= 2)) { // Will not split into more than 4 cells
-            var splitDist = (4 * (40 + (cell.getSpeed() * 4))) + (cell.getSize() * 1.75); // Distance needed to splitkill
-            var distToTarget = this.getAccDist(cell,this.target); // Distance between the target and this cell
-            // splitting condition
-            if (splitDist >= distToTarget) {
-            // Splitkill
-                console.log('[Bot] ' + cell.getName() + " has splitted")
-                this.gameServer.splitCells(this);
-            }
-        }
-    }
+    // Direction to move
+    var x1 = cell.position.x + (500 * Math.sin(angle));
+    var y1 = cell.position.y + (500 * Math.cos(angle));
 
-    feed() {
-        if ((!this.target) || (this.target.getType() == 0) || (this.visibleNodes.indexOf(this.target) == -1)) {
-            // Food is eaten/a player cell/out of sight... so find a new food cell to target
-            this.target = this.findNearest(cell,this.food);
-                    
-            this.mouse = {x: this.target.position.x, y: this.target.position.y};
-        }
-    }
-
-    wander() {
-        if ((cell.position.x == this.mouseX) && (cell.position.y == this.mouseY)) {
-            // Get a new position
-            var index = Math.floor(Math.random() * this.gameServer.nodes.length);
-            var randomNode = this.gameServer.nodes[index];
-            var pos = {x: 0, y: 0};
+    if ((!this.target) || (this.target.getType() == 0) || (this.visibleNodes.indexOf(this.target) == -1)) {
+        var foods = this.getFoodBox(x1,y1);
+        if (foods) {
+            this.target = this.findNearest(cell, this.food);
     
-            if (randomNode.getType() == 3 | 1) {
-                pos.x = randomNode.position.x;
-                pos.y = randomNode.position.y;
-            } else {
-                // Not a food/ejected cell
-                pos = this.gameServer.getRandomPosition();
-            }
-    
-            // Set bot's mouse coords to this location
-            this.mouse = {x: pos.x, y: pos.y};
+            this.mouseX = this.target.position.x;
+            this.mouseY = this.target.position.y;
         }
-    }
+    }   
 
-    // checkers
+    this.mouse = {x: x1, y: y1};
 }
+
+BTBot.fight = function(){
+    if ((!this.target) || (this.visibleNodes.indexOf(this.target) == -1)) {
+        this.target = this.getRandom(this.prey);
+    }
+    //console.log("[Bot] "+cell.getName()+": Targeting "+this.target.getName());
+                
+    this.mouse = {x: this.target.position.x, y: this.target.position.y};
+
+    var massReq = 1.25 * (this.target.mass * 2 ); // Mass required to splitkill the target
+
+    if ((cell.mass > massReq) && (this.cells.length <= 2)) { // Will not split into more than 4 cells
+        var splitDist = (4 * (40 + (cell.getSpeed() * 4))) + (cell.getSize() * 1.75); // Distance needed to splitkill
+        var distToTarget = this.getAccDist(cell,this.target); // Distance between the target and this cell
+        // splitting condition
+        if (splitDist >= distToTarget) {
+        // Splitkill
+            console.log('[Bot] ' + cell.getName() + " has splitted")
+            this.gameServer.splitCells(this);
+        }
+    }
+}
+
+BTBot.feed = function(){
+    if ((!this.target) || (this.target.getType() == 0) || (this.visibleNodes.indexOf(this.target) == -1)) {
+        // Food is eaten/a player cell/out of sight... so find a new food cell to target
+        this.target = this.findNearest(cell,this.food);
+                
+        this.mouse = {x: this.target.position.x, y: this.target.position.y};
+    }
+}
+
+BTBot.wander = function(){
+    if ((cell.position.x == this.mouseX) && (cell.position.y == this.mouseY)) {
+        // Get a new position
+        var index = Math.floor(Math.random() * this.gameServer.nodes.length);
+        var randomNode = this.gameServer.nodes[index];
+        var pos = {x: 0, y: 0};
+
+        if (randomNode.getType() == 3 | 1) {
+            pos.x = randomNode.position.x;
+            pos.y = randomNode.position.y;
+        } else {
+            // Not a food/ejected cell
+            pos = this.gameServer.getRandomPosition();
+        }
+
+        // Set bot's mouse coords to this location
+        this.mouse = {x: pos.x, y: pos.y};
+    }
 
 // Behavior Tree
 BehaviorTree.register('flee', new Task({
-    run: function (behavior_bot) {
-        behavior_bot.flee()
+    run: function (BTBot) {
+        BTBot.flee()
         return SUCCESS
     }
 }))
 
 BehaviorTree.register('fight', new Task({
-    run: function (behavior_bot) {
-        behavior_bot.fight()
+    run: function (BTBot) {
+        BTBot.fight()
         return SUCCESS
     }
 }))
 
 BehaviorTree.register('feed', new Task({
-    run: function (behavior_bot) {
-        behavior_bot.feed()
+    run: function (BTBot) {
+        BTBot.feed()
         return SUCCESS
     }
 }))
 
 BehaviorTree.register('wander', new Task({
-    run: function (behavior_bot) {
-        behavior_bot.wander()
+    run: function (BTBot) {
+        BTBot.wander()
         return SUCCESS
     }
 }))
@@ -142,7 +135,7 @@ const tree = new Selector({
         'flee',
         // flee
         new Task({
-            run: function(behavior_bot) {
+            run: function(BTBot) {
                 if (this.gameState == 2) { 
                     // flee will call the above function and return success
                     //'flee'
@@ -158,7 +151,7 @@ const tree = new Selector({
         
         // fight
         new Task({
-            run: function(behavior_bot) {
+            run: function(BTBot) {
                 if (this.gameState == 3) { 
                     // fight will call the above function and return success
                     //'fight'
@@ -167,7 +160,7 @@ const tree = new Selector({
                         //case 1: SPLIT - smaller enemy, split towards to catch and consume
                         
                         //case 2: NO SPLIT - chase enemy, follow until consumed or gets too 
-                    behavior_bot.fight()
+                        BTBot.fight()
                     return SUCCESS
                     
                 } else {
@@ -178,12 +171,12 @@ const tree = new Selector({
         
         // feed
         new Task({
-            run: function(behavior_bot) {
+            run: function(BTBot) {
                 if (this.gameState == 1) { 
                     // feed will call the above function and return success
                     //'feed'
                     console.log('[Bot] ' + cell.getName() + " has fed")
-                    behavior_bot.feed()
+                    BTBot.feed()
                     return SUCCESS
                 } else {
                     return FAILURE
@@ -193,12 +186,12 @@ const tree = new Selector({
         
         // wander - default
         new Task({
-            run: function(behavior_bot) {
+            run: function(BTBot) {
                 if (this.gameState == 0) { 
                     // flee will call the above function and return success
                     // 'wander'
                     console.log('[Bot] ' + cell.getName() + " has wandered")
-                    behavior_bot.wander()
+                    BTBot.wander()
                     return SUCCESS
                 } else {
                     return FAILURE
@@ -337,10 +330,10 @@ BTBot.prototype.decide = function(cell) {
 
     //const intitalbot = new BehaviorBot()
     //const behavior_bot = Object.assign(cell, intitalbot)
-    const behavior_bot = new BehaviorBot()
+
     const bTree = new BehaviorTree({
         tree: tree,
-        blackboard: behavior_bot
+        blackboard: BTBot
     })
     console.log("_____________________________________________")
     // The "game" loop:
@@ -428,6 +421,6 @@ BTBot.prototype.getFoodBox = function(x,y) {
           
 		list.push(check);
     }
+}   
 }
-
 // Custom
